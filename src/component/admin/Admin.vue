777323129b459df35846fc36c27e07d6aa9b71a0
PengFei <626138959@qq.com>
2017-12-16 17:39:14 +0800

    创建后台管理页面目录结构与路由修改

---
+++ src/component/admin/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <!-- 页面左侧是公共的导航 -->
+        <section>
+            <aside></aside>
+        </section>
+        
+        <!-- 页面右侧 -->
+        <section>
+            <!-- 头部是公共的 -->
+            <header></header>
+            <!-- 这一块是动态变化的, 将来通过子路由控制 -->
+            <article></article>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,18 @@
 // 导入被路由的组件
 import HomeComponent from '../component/home/Home.vue';
 import LoginComponent from '../component/login/Login.vue';
+import AdminComponent from '../component/admin/Admin.vue';
 
 // 对外导出路由配置对象
 export default {
     routes: [
-        // 首页
-        { name: 'h', path: '/home', component: HomeComponent },
+        // 后台管理首页
+        { name: 'h', path: '/', component: AdminComponent, children: [
+            
+        ]},
 
         // 账号管理
         { name: 'l', path: '/login', component: LoginComponent },
+
     ]
 };
