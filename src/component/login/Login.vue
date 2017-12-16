702b72335e0faec65025d017072558acac31e884
PengFei <626138959@qq.com>
2017-12-16 17:38:38 +0800

    跳转登陆页样式, 添加表单校验

--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -10,7 +10,8 @@
             <!-- 表单 -->
             <section class="login_content_form">
                 <!-- form: 如果你的表单要进行效验, 重置, 那么必须设置model属性, 关联整个表单的数据 -->
-                <el-form :model="user" status-icon ref="ruleForm2" label-width="100px" class="demo-ruleForm">
+                <el-form :model="user" :rules="formRules" status-icon ref="ruleForm2" label-width="70px"
+                    class="demo-ruleForm" label-position="left">
                     
                     <!-- input: 如果要效验, 重置, 那么必须设置prop属性, 关联当前的input字段名 -->
                     <el-form-item label="用户名" prop="uname">
@@ -25,7 +26,7 @@
                     </el-form-item>
 
                     <el-form-item>
-                        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+                        <el-button @click="submitForm('ruleForm2')">提交</el-button>
                         <el-button @click="resetForm('ruleForm2')">重置</el-button>
                     </el-form-item>
                 </el-form>
@@ -42,6 +43,18 @@
                 user: {
                     uname: '',
                     upwd: ''
+                },
+
+                // 配置校验规则
+                formRules: {
+                    uname: [
+                        { required: true, message: '请输入用户名', trigger: 'blur' },
+                        { pattern: /\w{5,18}/, message: '长度在 4 到 18 个字符, 且只能为&_或字母与数字', trigger: 'blur' },
+                    ],
+                    upwd: [
+                        { required: true, message: '请输入密码', trigger: 'blur' },
+                        { pattern: /.{6,18}/, message: '长度在 6 到 18 个字符', trigger: 'blur' },
+                    ]
                 }
             };
         },
@@ -50,12 +63,22 @@
 
             // 点击提交按钮, 调用登陆接口
             submitForm(formName) {
-                this.$http.post(this.$api.login, this.user)
-                    .then(rsp => alert(rsp.data.message.realname));
+
+                // 先校验表单, 再提交
+                this.$refs[formName].validate((result) => {
+                    if(result) {
+                        this.$http.post(this.$api.login, this.user)
+                            .then(rsp => alert(rsp.data.message.realname));
+                    }else {
+                        alert('哥们你逗我呢!')
+                    }
+                });
             },
 
+            // vue中提供了一个$refs对象, 用来快速的获取dom元素, 
+            // 在一些特殊情况下我们需要拿到原生dom进行操作
             resetForm(formName) {
-                this.$refs[formName].resetFields();
+                this.$refs[formName].resetFields();   // 这里拿到form组件对象, 调用其重置方法重置表单
             }
         }
     }
@@ -64,18 +87,20 @@
 <style scoped lang="less">
     .login {
         height: 100%;
-        background-color: blue;
+        background-color: rgb(38, 124, 183);
 
         // &符号就是上级选择器的引用
         // 这里使盒子处于页面中间位置
         &_content {
             width: 400px;
-            position: relative;
+            position: absolute;
             left: 50%;
             top: 50%;
             margin-left: -200px;
             margin-top: -160px;
-            border: 1px solid red;
+            padding: 10px;
+            border: 1px solid hsla(0,0%,100%,.2);
+            border-radius: 4px;
 
             // logo居中
             &_logo {
@@ -84,7 +109,7 @@
 
             // form表单使用elemUI
             &_form {
-
+                padding: 2px 10px;
             }
         }
     }
